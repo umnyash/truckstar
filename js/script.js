0521308cdb9bdf74d4f939de773f985e3da7617e
umnyash <63366308+umnyash@users.noreply.github.com>
2024-07-11 23:36:38 +0000

    Deploying to gh-pages from @ umnyash/truckstar@ba1a2a9052d9187b9824c5cdc52c8c3cf7c01e09 🚀

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
 "use strict";
+
+/* * * * * * * * * * * * * * * * * * * * * * * *
+ * banner-slider.js
+ */
+function initBannerSlider(sliderElement) {
+  new Swiper(sliderElement, {
+    effect: 'fade',
+    fadeEffect: {
+      crossFade: true
+    },
+    loop: true,
+    speed: 1400,
+    allowTouchMove: false,
+    autoplay: {
+      delay: 1000,
+      disableOnInteraction: false
+    }
+  });
+}
+/* * * * * * * * * * * * * * * * * * * * * * * */
+
+/* * * * * * * * * * * * * * * * * * * * * * * *
+ * main.js
+ */
+
+document.querySelectorAll('.banner-slider').forEach(initBannerSlider);
+
+/* * * * * * * * * * * * * * * * * * * * * * * */
