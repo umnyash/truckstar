6ad4c6e938cad565397813230b965ff9b00ccb79
umnyash <63366308+umnyash@users.noreply.github.com>
2024-07-29 07:26:19 +0000

    Deploying to gh-pages from @ umnyash/truckstar@1373afe9b0e26c59befcd14ffa4d85029d532dea 🚀

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
 "use strict";
 
+function getPaginationButtonCreator() {
+  let slideName = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'Слайд';
+  return (index, className) => `
+    <button class='${className}' type='button'>
+      <span class='visually-hidden'>${slideName} ${index + 1}.</span>
+    </button>
+  `;
+}
+
 /* * * * * * * * * * * * * * * * * * * * * * * *
  * banner-slider.js
  */
@@ -61,6 +70,52 @@ const initFolds = foldsElement => {
     }
   });
 };
+/* * * * * * * * * * * * * * * * * * * * * * * */
+
+/* * * * * * * * * * * * * * * * * * * * * * * *
+ * photo-slider.js
+ */
+function initPhotoSlider(sliderElement) {
+  const mainSliderElement = sliderElement.querySelector('.photo-slider__main-slider');
+  const thumbnailsSliderElement = sliderElement.querySelector('.photo-slider__thumbnails-slider');
+  const thumbnailsSwiper = new Swiper(thumbnailsSliderElement, {
+    watchSlidesProgress: true,
+    slidesPerView: 3,
+    spaceBetween: 8,
+    spaceBetween: 10,
+    breakpoints: {
+      768: {
+        slidesPerView: 6,
+        spaceBetween: 8
+      },
+      1366: {
+        slidesPerView: 6,
+        spaceBetween: 12
+      },
+      1920: {
+        slidesPerView: 6,
+        spaceBetween: 20
+      }
+    }
+  });
+  new Swiper(mainSliderElement, {
+    effect: 'fade',
+    fadeEffect: {
+      crossFade: true
+    },
+    pagination: {
+      el: '.photo-slider__main-slider-pagination',
+      bulletClass: 'photo-slider__pagination-button',
+      bulletActiveClass: 'photo-slider__pagination-button--current',
+      renderBullet: getPaginationButtonCreator(),
+      clickable: true
+    },
+    thumbs: {
+      swiper: thumbnailsSwiper,
+      slideThumbActiveClass: 'photo-slider__thumbnails-slider-item--active'
+    }
+  });
+}
 /* * * * * * * * * * * * * * * * * * * * * * * */
 
 /* * * * * * * * * * * * * * * * * * * * * * * *
@@ -154,4 +209,5 @@ document.querySelectorAll('.folds').forEach(initFolds);
 document.querySelectorAll('.simple-form').forEach(initSimpleForm);
 document.querySelectorAll('.products').forEach(initProductsSlider);
 document.querySelectorAll('.process').forEach(initProcessSlider);
+document.querySelectorAll('.photo-slider').forEach(initPhotoSlider);
 /* * * * * * * * * * * * * * * * * * * * * * * */
