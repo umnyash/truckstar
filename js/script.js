dbd8493208e3d541827a583863e10a4cbfdd34a3
umnyash <63366308+umnyash@users.noreply.github.com>
2024-08-01 06:05:57 +0000

    Deploying to gh-pages from @ umnyash/truckstar@1eb9305f895ba434dabac43ce630ebaec06d7b1f 🚀

--- js/script.js
+++ js/script.js
@@ -212,6 +212,22 @@ function initProductsSlider(sliderWrapperElement) {
 /* * * * * * * * * * * * * * * * * * * * * * * */
 
 /* * * * * * * * * * * * * * * * * * * * * * * *
+ * select.js
+ */
+function initSelect(selectElement) {
+  new Choices(selectElement, {
+    // eslint-disable-line
+    searchEnabled: false,
+    itemSelectText: '',
+    shouldSort: false
+    // maxItemCount: 4,
+    // placeholder: true,
+    // placeholderValue: 'fff',
+  });
+}
+/* * * * * * * * * * * * * * * * * * * * * * * */
+
+/* * * * * * * * * * * * * * * * * * * * * * * *
  * simple-form.js
  */
 function initSimpleForm(formElement) {
@@ -233,6 +249,42 @@ function initSimpleForm(formElement) {
 /* * * * * * * * * * * * * * * * * * * * * * * */
 
 /* * * * * * * * * * * * * * * * * * * * * * * *
+ * text-area.js
+ */
+function initTextArea(fieldElement) {
+  const controlElement = fieldElement.querySelector('.text-area__control');
+  const updateEmptyStatus = () => {
+    fieldElement.classList.toggle('text-area--empty', !controlElement.value);
+  };
+  updateEmptyStatus();
+  controlElement.addEventListener('input', () => {
+    updateEmptyStatus();
+  });
+}
+/* * * * * * * * * * * * * * * * * * * * * * * */
+
+/* * * * * * * * * * * * * * * * * * * * * * * *
+ * text-field.js
+ */
+function initTextField(fieldElement) {
+  const controlElement = fieldElement.querySelector('.text-field__control');
+  const clearButtonElement = fieldElement.querySelector('.text-field__button--clear');
+  const updateEmptyStatus = () => {
+    fieldElement.classList.toggle('text-field--empty', !controlElement.value);
+  };
+  updateEmptyStatus();
+  controlElement.addEventListener('input', () => {
+    updateEmptyStatus();
+  });
+  clearButtonElement.addEventListener('click', () => {
+    controlElement.value = '';
+    updateEmptyStatus();
+    controlElement.focus();
+  });
+}
+/* * * * * * * * * * * * * * * * * * * * * * * */
+
+/* * * * * * * * * * * * * * * * * * * * * * * *
  * main.js
  */
 document.querySelectorAll('.banner-slider').forEach(initBannerSlider);
@@ -242,4 +294,7 @@ document.querySelectorAll('.products').forEach(initProductsSlider);
 document.querySelectorAll('.process').forEach(initProcessSlider);
 document.querySelectorAll('.photo-slider').forEach(initPhotoSlider);
 document.querySelectorAll('.checker-cards__list').forEach(initCheckerCardsList);
+document.querySelectorAll('.text-field').forEach(initTextField);
+document.querySelectorAll('.text-area').forEach(initTextArea);
+document.querySelectorAll('.select').forEach(initSelect);
 /* * * * * * * * * * * * * * * * * * * * * * * */
