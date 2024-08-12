7407d56dd8b9a55c6e4e9ad30d0b775396db36b2
umnyash <63366308+umnyash@users.noreply.github.com>
2024-08-12 07:31:32 +0000

    Deploying to gh-pages from @ umnyash/truckstar@b7cdd99bea7d3e9bde191c4b1f45a1989e4da296 🚀

--- js/script.js
+++ js/script.js
@@ -946,6 +946,29 @@ function initProcessSlider(sliderWrapperElement) {
 /* * * * * * * * * * * * * * * * * * * * * * * */
 
 /* * * * * * * * * * * * * * * * * * * * * * * *
+ * product-navigation.js
+ */
+function initProductNavigation(navigationElement) {
+  const siteHeaderElement = document.querySelector('.page__site-header');
+  navigationElement.addEventListener('click', evt => {
+    evt.preventDefault();
+    const id = evt.target.getAttribute('href');
+    const targetElement = document.querySelector(id);
+    if (targetElement) {
+      const targetElementPosition = targetElement.getBoundingClientRect().top + window.scrollY;
+      window.scrollTo({
+        top: targetElementPosition - siteHeaderElement.offsetHeight - 20,
+        behavior: 'smooth'
+      });
+      if (targetElement.matches('.folds__item:not(.folds__item--open)')) {
+        targetElement.querySelector('.folds__button').click();
+      }
+    }
+  });
+}
+/* * * * * * * * * * * * * * * * * * * * * * * */
+
+/* * * * * * * * * * * * * * * * * * * * * * * *
  * products-slider.js
  */
 function initProductsSlider(sliderWrapperElement) {
@@ -1178,6 +1201,7 @@ document.querySelectorAll('[data-modal="locations"]').forEach(modalElement => {
   initLocationsModal(modalElement);
 });
 document.querySelectorAll('.modal--with_document').forEach(initDocumentModal);
+document.querySelectorAll('.product .page-navigation').forEach(initProductNavigation);
 let requestForm = null;
 const requestFormElement = document.querySelector('.request-form');
 if (requestFormElement) {
