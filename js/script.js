d9f2e2e1f1e946312d61095116d9663f285ed26a
umnyash <63366308+umnyash@users.noreply.github.com>
2024-08-14 14:02:30 +0000

    Deploying to gh-pages from @ umnyash/truckstar@97c9fad870e097d2985e7497b3f08b78585f84c8 🚀

--- js/script.js
+++ js/script.js
@@ -92,6 +92,30 @@ async function sendData(url, body) {
 }
 /* * * * * * * * * * * * * * * * * * * * * * * */
 
+class PubSub {
+  constructor() {
+    this.listeners = {};
+  }
+  addListener(event, fn) {
+    if (!this.listeners[event]) {
+      this.listeners[event] = [];
+    }
+    this.listeners[event].push(fn);
+  }
+  removeListener(event, fn) {
+    if (!this.listeners[event]) {
+      return;
+    }
+    this.listeners[event] = this.listeners[event].filter(listener => listener !== fn);
+  }
+  emit(event, data) {
+    if (!this.listeners[event]) {
+      return;
+    }
+    this.listeners[event].forEach(listener => listener(data));
+  }
+}
+
 /* * * * * * * * * * * * * * * * * * * * * * * *
  * modal.js
  */
